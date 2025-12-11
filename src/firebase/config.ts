03d5943a92a8bb1c3bf2eb60a856a2cef01b73f6
RSU CRAIITECH <craiitech@rsu.edu.ph>
2025-12-11 15:40:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9288801258-68151",
+  "appId": "1:60303767170:web:e3a63661037539f80ca69b",
+  "apiKey": "AIzaSyBV89dKZPYpXnuceUCZ86MQ5EbOtEXHuvs",
+  "authDomain": "studio-9288801258-68151.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "60303767170"
+};
