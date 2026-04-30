b2d96f56d3df15021959c81b10d29d8e94d15254
RSU CRAIITECH <craiitech@rsu.edu.ph>
2026-04-30 07:59:20 +0000

    1/1
    Next.js 15.5.9 (outdated)
    Turbopack
    Runtime ReferenceError
    
    Label

--- src/components/gad/gad-activities-tab.tsx
+++ src/components/gad/gad-activities-tab.tsx
@@ -26,6 +26,7 @@ import {
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
